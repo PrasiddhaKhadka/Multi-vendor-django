6cc0a0b3964e3a53b59fe9625b99077762075f68
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-09-16 13:10:09 +0545

    Add: Custom User Model

---
+++ accounts/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            # You cannot create a user without an email.
+            raise ValueError('The email must be set')
+        
+        # Converts emails into a standard format.
+        # Example: 'TEST@GMAIL.COM' → 'test@gmail.com'
+        email = self.normalize_email(email)
+        
+        
+        # self.model points to your User model (which you'll define later).
+         # It initializes the user object with email and any extra fields (like first_name, etc.)
+        user = self.model(email=email, **extra_fields)
+
+        user.set_password(password) # Hashes the password (never stores raw password).
+
+
+        #self.db will be using default database
+        user.save(using = self._db)                 # Saves user in the database. 
+
+
+        return user
+    
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(
+            email= self.normalize_email(email),
+            password=password
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+       
+class User(AbstractBaseUser):
+
+    RESTAURANT = 1
+    CUSTOMER = 2
+
+    ROLE_CHOICES = (
+        (RESTAURANT, 'Restaurant'),
+        (CUSTOMER, 'Customer')
+    )
+    email = models.EmailField(verbose_name='email', max_length=60, unique=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True)
+
+    # required roles 
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    
+    USERNAME_FIELD = 'email'
+    # donnt need to place the userfield name into required fields because it itself lies in required fields 
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
+
--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-frvt9b6d9s3e8j*0p@cz8sty(21k9h0^50lpi6f3ok*&u$9=56'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +79,28 @@ WSGI_APPLICATION = 'foodOnline_main.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # alias default
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
+
+    # incase double db is needed-> alias would have been 'replica'
+    # 'replica': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'replicadb',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'password',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
-
+AUTH_USER_MODEL = 'accounts.User'
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -115,6 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'foodOnline_main/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
