e299db078b8133a0c960e68878824ce70d814103
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-09-17 16:39:35 +0545

    Add: Vendor Admin and Signals

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from . models import Vendor
-# from  utils.email_utils import send_vendor_approval_notification    
-# Register your models here.
+
+
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
     list_display = ('vendor_name', 'is_approved','created_at',)
@@ -9,29 +9,3 @@ class VendorAdmin(admin.ModelAdmin):
     list_filter = ['is_approved', 'created_at']
     search_fields = ['vendor_name', 'vendor_user__email']
     readonly_fields = ['created_at', 'modified_at']
-
-    # actions = ['approve_vendors', 'reject_vendors']
-
-    # def approve_vendors(self, request, queryset):
-    #     """Custom admin action to approve vendors"""
-    #     updated = 0
-    #     for vendor in queryset.filter(is_approved=False):
-    #         vendor.is_approved = True
-    #         vendor.save()
-    #         send_vendor_approval_notification(vendor, approved=True)
-    #         updated += 1
-        
-    #     self.message_user(request, f'{updated} vendors were approved and notified.')
-    # approve_vendors.short_description = "Approve selected vendors"
-    
-    # def reject_vendors(self, request, queryset):
-    #     """Custom admin action to reject vendors"""
-    #     updated = 0
-    #     for vendor in queryset.filter(is_approved=True):
-    #         vendor.is_approved = False
-    #         vendor.save()
-    #         send_vendor_approval_notification(vendor, approved=False)
-    #         updated += 1
-        
-    #     self.message_user(request, f'{updated} vendors were rejected and notified.')
-    # reject_vendors.short_description = "Reject selected vendors"
--- vendor/signals.py
+++ vendor/signals.py
@@ -4,16 +4,36 @@ from .models import Vendor
 
 # Signal to notify admin when vendor registers
 @receiver(post_save, sender=Vendor)
+# instance is acutally the object of vendor 
+# vendor = Vendor(
+#     vendor_name="ShopX",
+#     is_approved=False,
+#     vendor_user=user,
+#     vendor_profile=profile
+
+# )
+
 def vendor_register_notification_to_admin(sender, instance, created, *args, **kwargs):
     if created:
         from utils.email_utils import send_vendor_registration_notification_to_admin
         send_vendor_registration_notification_to_admin(instance)
+    else:
+        if not created:
+            # Check if is_approved field has changed
+            if instance.tracker.has_changed('is_approved') and instance.is_approved:
+                # Import here to avoid circular imports
+                from utils.email_utils import send_vendor_approval_notification
+                send_vendor_approval_notification(instance, approved=True)
 
-@receiver(post_save, sender=Vendor)
-def vendor_register_notification_to_customer(sender, instance, created, *args, **kwargs):
-    if not created:
-        # Check if is_approved field has changed
-        if instance.tracker.has_changed('is_approved') and instance.is_approved:
-            # Import here to avoid circular imports
-            from utils.email_utils import send_vendor_approval_notification
-            send_vendor_approval_notification(instance, approved=True)
+
+
+# Duplication of Code
+
+# @receiver(post_save, sender=Vendor)
+# def vendor_register_notification_to_customer(sender, instance, created, *args, **kwargs):
+#     if not created:
+#         # Check if is_approved field has changed
+#         if instance.tracker.has_changed('is_approved') and instance.is_approved:
+#             # Import here to avoid circular imports
+#             from utils.email_utils import send_vendor_approval_notification
+#             send_vendor_approval_notification(instance, approved=True)
